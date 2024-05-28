2d2ac350c5120e2ebbb6f738b49d2c6dd4ffbe15
qasimfaiz <faizqasim670@gmail.com>
2024-05-28 22:21:33 +0500

    change base url

--- Frontend/src/RequestApiCalls/Request.js
+++ Frontend/src/RequestApiCalls/Request.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "https://shopwiki-backend.vercel.app/";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const publicRequest = axios.create({
   baseURL: `${BASE_URL}/api/`,
@@ -12,40 +11,7 @@ export const publicRequest = axios.create({
   withCredentials: true,
 });
 
-export const userRequest =
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-axios.create({
+export const userRequest = axios.create({
   baseURL: `${BASE_URL}/api/`,
   headers: {
     "Content-type": "application/json",
